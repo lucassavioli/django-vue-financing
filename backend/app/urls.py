e2e2ae28331279cca60bba25e4c2237e2b379a1b
Lucas Savioli <lucassavioli@gmail.com>
2024-03-15 16:15:37 -0300

    Implemented API to fetch SAC and PRICE in a single request

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("sac/", views.SacView.as_view()),
-    path("price/", views.PriceView.as_view()),
+    path("sac/", views.SacView.as_view(), name="sac"),
+    path("price/", views.PriceView.as_view(), name="price"),
+    path("sacprice/", views.SacPriceView.as_view(), name="sacprice"),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,8 +1,9 @@
+from app.services import price, sac
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from app.services import sac, price
-from .serializers import SacSerializer, PriceSerializer
+
+from .serializers import PriceSerializer, SacSerializer
 
 
 class SacView(APIView):
@@ -37,3 +38,32 @@ class PriceView(APIView):
         else:
             # If serialization fails, return appropriate error response
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SacPriceView(APIView):
+    def post(self, request, format=None):
+        input_data = request.data
+        sac_table = sac(
+            input_data.get("loan"),
+            input_data.get("installments_number"),
+            input_data.get("interest"),
+        )
+        price_table = price(
+            input_data.get("loan"),
+            input_data.get("installments_number"),
+            input_data.get("interest"),
+        )
+        sac_serializer = SacSerializer(data=sac_table, many=True)
+        price_serializer = PriceSerializer(data=price_table, many=True)
+        if sac_serializer.is_valid() and price_serializer.is_valid():
+            sac_price = {"sac": sac_serializer.data, "price": price_serializer.data}
+            return Response(sac_price, status=status.HTTP_200_OK)
+        else:
+            # If serialization fails, return appropriate error response
+            return Response(
+                {
+                    "sac": sac_serializer.errors,
+                    "price": price_serializer.errors,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
