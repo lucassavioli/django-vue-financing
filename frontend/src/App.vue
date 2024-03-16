ac649eda3de44aa094a7a72dba8a39045a7df15d
Lucas Savioli <lucassavioli@gmail.com>
2024-03-16 15:35:18 -0300

    Added Flowbite UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,68 +1,81 @@
 <template>
-  <div class="container">
-    <div class="left-side">
-      <form @submit.prevent="submitForm">
-        <div>
-          <label for="loan">Loan:</label>
-          <input type="number" id="loan" v-model="loan" required>
+  <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div class="lg:col-span-6">
+      <h1>SAC PRICE Simulator</h1>
+      <form @submit.prevent="submitForm" class="max-w-sm mt-52">
+        <div class="mb-5">
+          <label for="loan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Loan:</label>
+          <input type="number" id="loan" v-model="loan"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required>
         </div>
-        <div>
-          <label for="installments_number">Installments Number:</label>
-          <input type="number" id="installments_number" v-model="installments_number" required>
+        <div class="mb-5">
+          <label for="installments_number"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Installments Number:</label>
+          <input type="number" id="installments_number" v-model="installments_number"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required>
         </div>
-        <div>
-          <label for="interest">Interest:</label>
-          <input type="number" step="0.01" id="interest" v-model="interest" required>
+        <div class="mb-5">
+          <label for="interest" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Interest:</label>
+          <input type="number" step="0.01" id="interest" v-model="interest"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required>
         </div>
-        <button type="submit">Submit</button>
+        <div class="mb-5">
+          <button type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Simulate</button>
+        </div>
       </form>
     </div>
-    <div class="right-side">
+    <div class="lg:mt-52 lg:col-span-6 lg:flex">
       <canvas id="amortizationChart"></canvas>
     </div>
-    <div v-if="data">
+    <div v-if="data" class="lg:col-span-12 flex gap-24 mt-16">
       <div>
         <h1>Sac Table</h1>
-        <table>
-          <thead>
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th>Installments Number</th>
-              <th>Installment</th>
-              <th>Interest</th>
-              <th>Amortization</th>
-              <th>Balance Due</th>
+              <th scope="col" class="px-6 py-3">Installments Number</th>
+              <th scope="col" class="px-6 py-3">Installment</th>
+              <th scope="col" class="px-6 py-3">Interest</th>
+              <th scope="col" class="px-6 py-3">Amortization</th>
+              <th scope="col" class="px-6 py-3">Balance Due</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in data.sac" :key="item.installments_number">
-              <td>{{ item.installments_number }}</td>
-              <td>{{ item.installment }}</td>
-              <td>{{ item.interest }}</td>
-              <td>{{ item.amortization }}</td>
-              <td>{{ item.balance_due }}</td>
+            <tr v-for="item in data.sac" :key="item.installments_number"
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td scope="row" class="px-6 py-4">{{ item.installments_number }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.installment }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.interest }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.amortization }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.balance_due }}</td>
             </tr>
           </tbody>
         </table>
       </div>
       <div>
         <h1>Price Table</h1>
-        <table>
-          <thead>
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th>Installments Number</th>
-              <th>Installment</th>
-              <th>Interest</th>
-              <th>Amortization</th>
-              <th>Balance Due</th>
+              <th scope="col" class="px-6 py-3">Installments Number</th>
+              <th scope="col" class="px-6 py-3">Installment</th>
+              <th scope="col" class="px-6 py-3">Interest</th>
+              <th scope="col" class="px-6 py-3">Amortization</th>
+              <th scope="col" class="px-6 py-3">Balance Due</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in data.price" :key="item.installments_number">
-              <td>{{ item.installments_number }}</td>
-              <td>{{ item.installment }}</td>
-              <td>{{ item.interest }}</td>
-              <td>{{ item.amortization }}</td>
-              <td>{{ item.balance_due }}</td>
+            <tr v-for="item in data.price" :key="item.installments_number"
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td scope="row" class="px-6 py-4">{{ item.installments_number }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.installment }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.interest }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.amortization }}</td>
+              <td scope="row" class="px-6 py-4">{{ item.balance_due }}</td>
             </tr>
           </tbody>
         </table>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
-import '.assets/index.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import './assets/index.css'
 
 createApp(App).mount('#app')
